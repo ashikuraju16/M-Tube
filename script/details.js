ba1e020aadbbeff5c315003b12f734faecaf1996
ashikuraju16 <ashikur.r.raju@gmail.com>
2024-03-10 19:20:51 +0600

    index

--- script/details.js
+++ script/details.js
@@ -1,18 +1,20 @@
+let currentCategoryId = 1000;
+
 const loadingDetails = async () => {
   const res = await fetch(
     " https://openapi.programming-hero.com/api/videos/categories"
   );
   const data = await res.json();
-  const phones = data.data;
-  displayItems(phones);
+  const categories = data.data;
+  displayItems(categories);
 };
 
-const displayItems = (phones) => {
-  console.log(phones);
+const displayItems = (categories) => {
+  console.log(categories);
   const buttonItems = document.getElementById("button-items");
   let currentButton = null;
 
-  phones.forEach((element) => {
+  categories.forEach((element) => {
     const button = document.createElement("button");
     button.classList = "btn text-xl";
     button.innerText = element.category;
@@ -31,13 +33,17 @@ const displayItems = (phones) => {
 
       // Set the current button to the clicked button
       currentButton = button;
+      console.log(element, "cb");
+      currentCategoryId = element.category_id;
+      itemsDetails();
     });
   });
 };
+const categoryButton = () => {};
 
 const itemsDetails = async () => {
   const res = await fetch(
-    "https://openapi.programming-hero.com/api/videos/category/1000"
+    `https://openapi.programming-hero.com/api/videos/category/${currentCategoryId}`
   );
   const data = await res.json();
   const items = data.data;
@@ -62,15 +68,15 @@ const displayCard = (items) => {
     item.authors[0].profile_picture
   }" alt="Shoes" /></div>
  <div class=" pl-3 gap-y-6 space-y-3">
- <h2 class="text-3xl font-extrabold max-sm:text-xl max-lg:text-2xl max-xl:text-2xl max-2xl:text-2xl">${item.title}</h2>
+ <h2 class="text-3xl font-extrabold max-sm:text-xl max-lg:text-2xl max-xl:text-2xl max-2xl:text-2xl">${
+   item.title
+ }</h2>
  <div class="flex gap-x-2">
  <h2 class="text-xl max-xl:text-lg">${item.authors[0].profile_name}</h2>
  <h2 class="text-xl text-blue-700">${
-   item.authors[0].verified === true ? (
-'<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="m23 12l-2.44-2.79l.34-3.69l-3.61-.82l-1.89-3.2L12 2.96L8.6 1.5L6.71 4.69L3.1 5.5l.34 3.7L1 12l2.44 2.79l-.34 3.7l3.61.82L8.6 22.5l3.4-1.47l3.4 1.46l1.89-3.19l3.61-.82l-.34-3.69zM9.38 16.01L7 13.61a.996.996 0 0 1 0-1.41l.07-.07c.39-.39 1.03-.39 1.42 0l1.61 1.62l5.15-5.16c.39-.39 1.03-.39 1.42 0l.07.07c.39.39.39 1.02 0 1.41l-5.92 5.94c-.41.39-1.04.39-1.44 0" /></svg>'
-   ) : (
-     ""
-   )
+   item.authors[0].verified === true
+     ? '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="m23 12l-2.44-2.79l.34-3.69l-3.61-.82l-1.89-3.2L12 2.96L8.6 1.5L6.71 4.69L3.1 5.5l.34 3.7L1 12l2.44 2.79l-.34 3.7l3.61.82L8.6 22.5l3.4-1.47l3.4 1.46l1.89-3.19l3.61-.82l-.34-3.69zM9.38 16.01L7 13.61a.996.996 0 0 1 0-1.41l.07-.07c.39-.39 1.03-.39 1.42 0l1.61 1.62l5.15-5.16c.39-.39 1.03-.39 1.42 0l.07.07c.39.39.39 1.02 0 1.41l-5.92 5.94c-.41.39-1.04.39-1.44 0" /></svg>'
+     : ""
  }</h2>
  </div>
  <h2 class="text-lg">${item.others.views}</h2>
